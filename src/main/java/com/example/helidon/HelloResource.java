5db44bd29befda86d91252d8c6cc15f86b69ba96
maksym <max>
2022-11-18 19:54:10 +0100

    mqtt+native image

--- src/main/java/com/example/helidon/HelloResource.java
+++ src/main/java/com/example/helidon/HelloResource.java
@@ -7,19 +7,24 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 @Path("/hello-world")
 public class HelloResource {
 
+//    @Inject
+//    @Channel("emit-it")
+//    private Emitter<String> emitter;
+
     @Inject
-    @Channel("emit-it")
-    private Emitter<String> emitter;
+    private Service service;
 
     @GET
     @Produces("text/plain")
-    public String hello(@QueryParam(value = "name") String name) {
+    public String hello(@QueryParam(value = "name") String name) throws MqttException {
         String response = String.format("Hello, %s!", name);
-        emitter.send(response);
+        service.publishMessage(response);
+//        emitter.send(response);
         return response;
     }
 }
--- src/main/java/com/example/helidon/JmsConnections.java
+++ src/main/java/com/example/helidon/JmsConnections.java
@@ -1,29 +1,29 @@
-package com.example.helidon;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
-import jakarta.inject.Named;
-import jakarta.jms.ConnectionFactory;
-import org.apache.qpid.jms.JmsConnectionFactory;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-
-
-@ApplicationScoped
-public class JmsConnections {
-
-    @Produces
-    @Named("activemq")
-    public ConnectionFactory connectionFactoryActiveMq(@ConfigProperty(name = "rmq.username") String username,
-                                                       @ConfigProperty(name = "rmq.password") String password,
-                                                       @ConfigProperty(name = "rmq.host") String host) {
-        return new JmsConnectionFactory(username, password, host);
-    }
-
-    @Incoming("from-jms")
-    @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING)
-    public void rabbit(String msg) {
-        System.out.println("JMS from SomeQueue says: " + msg);
-    }
-}
+//package com.example.helidon;
+//
+//import jakarta.enterprise.context.ApplicationScoped;
+//import jakarta.enterprise.inject.Produces;
+//import jakarta.inject.Named;
+//import jakarta.jms.ConnectionFactory;
+//import org.apache.qpid.jms.JmsConnectionFactory;
+//import org.eclipse.microprofile.config.inject.ConfigProperty;
+//import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
+//import org.eclipse.microprofile.reactive.messaging.Incoming;
+//
+//
+//@ApplicationScoped
+//public class JmsConnections {
+//
+//    @Produces
+//    @Named("activemq")
+//    public ConnectionFactory connectionFactoryActiveMq(@ConfigProperty(name = "rmq.username") String username,
+//                                                       @ConfigProperty(name = "rmq.password") String password,
+//                                                       @ConfigProperty(name = "rmq.host") String host) {
+//        return new JmsConnectionFactory(username, password, host);
+//    }
+//
+//    @Incoming("from-jms")
+//    @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING)
+//    public void rabbit(String msg) {
+//        System.out.println("JMS from SomeQueue says: " + msg);
+//    }
+//}
